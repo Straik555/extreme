fbc52712faa0879a62ab8236cbf0f6d55f55bd0a
Vitalii Zakharchuk <95skin95@gmail.com>
2020-12-16 11:48:24 +0200

    now

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {ThemeProvider} from "styled-components";
-
-import * as firebase from 'firebase';
-import App from './components/App';
-import store from "./store";
-import theme from './styles/theme';
-import ProductstoreService from './services';
-import {ProductstoreServiceProvider} from "./components/ProductServiceContext";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCFCZNM2Gr78RpyFE8jD-ro6oqYU7Km6xI",
-    authDomain: "fir-test-d943b.firebaseapp.com",
-    databaseURL: "https://fir-test-d943b.firebaseio.com",
-    projectId: "fir-test-d943b",
-    storageBucket: "fir-test-d943b.appspot.com",
-    messagingSenderId: "30561993070",
-    appId: "1:30561993070:web:98cdebbdcbd06cc4cb417f"
-};
-
-const productstoreService = new ProductstoreService();
-firebase.initializeApp(firebaseConfig);
+// import {Provider} from 'react-redux';
+// import {ThemeProvider} from "styled-components";
+//
+// import * as firebase from 'firebase';
+// import App from './components/App';
+// import store from "./store";
+// import theme from './styles/theme';
+// import ProductstoreService from './services';
+// import {ProductstoreServiceProvider} from "./components/ProductServiceContext";
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCFCZNM2Gr78RpyFE8jD-ro6oqYU7Km6xI",
+//     authDomain: "fir-test-d943b.firebaseapp.com",
+//     databaseURL: "https://fir-test-d943b.firebaseio.com",
+//     projectId: "fir-test-d943b",
+//     storageBucket: "fir-test-d943b.appspot.com",
+//     messagingSenderId: "30561993070",
+//     appId: "1:30561993070:web:98cdebbdcbd06cc4cb417f"
+// };
+//
+// const productstoreService = new ProductstoreService();
+// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-    <Provider store={store} >
-        <ProductstoreServiceProvider value={productstoreService} >
-            <ThemeProvider theme={theme}>
-               <App />
-            </ThemeProvider>
-        </ProductstoreServiceProvider>
-    </Provider>
-        ,
+    // <Provider store={store} >
+    //     <ProductstoreServiceProvider value={productstoreService} >
+    //         <ThemeProvider theme={theme}>
+    //            <App />
+    //         </ThemeProvider>
+    //     </ProductstoreServiceProvider>
+    // </Provider>
+    <h1>Hell</h1>,
   document.getElementById('root')
 );
 
